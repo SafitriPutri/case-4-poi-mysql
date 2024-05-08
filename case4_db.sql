d0187d7d75f81664537f8f95ba32baef696a9150
SafitriPutri <117289241+SafitriPutri@users.noreply.github.com>
2024-05-08 19:55:37 +0700

    case4_db.sql

--- case4_db.sql
+++ case4_db.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 08, 2024 at 11:20 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `case4_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `poi`
---
 
 CREATE TABLE `poi` (
   `id` int(11) NOT NULL,
@@ -38,35 +14,15 @@ CREATE TABLE `poi` (
   `alamat` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `poi`
---
 
 INSERT INTO `poi` (`id`, `nama`, `deskripsi`, `latitude`, `longitude`, `pulau`, `no_rmh`, `alamat`) VALUES
 (27, 'Ann', 'Kos', '-7.9618092659037645', '112.62917619034673', 'Jawa', 15, 'Jl Soekarno Hatta'),
 (28, 'Ann', 'Kos', '-7.960960503486977', '112.62653485427032', '123', 3, 'Jl Soekarno Hatta');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `poi`
---
 ALTER TABLE `poi`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `poi`
---
 ALTER TABLE `poi`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
